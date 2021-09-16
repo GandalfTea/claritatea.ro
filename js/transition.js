8cd3363e55819ba9479265eab789e7287eefb5fc
Octavian Rusu <octavian.concept@gmail.com>
2021-09-16 00:55:20 -0600

    add first images / modified top menu / added submit button

--- js/transition.js
+++ js/transition.js
@@ -7,11 +7,15 @@ function searchCollapse() {
 			$(".search__input").show();
 		}, 500);
 		$(".top-menu__search").removeClass("top-menu__search--collapsed");
+		$(".top-menu__buttons").removeClass("top-menu__buttons--collapsed");
 		$(".search__icon").removeClass("search__icon--collapsed");
+		$("#buttons__despre").show();
 	} else {
 		$(".top-menu__search").addClass("top-menu__search--collapsed");
+		$(".top-menu__buttons").addClass("top-menu__buttons--collapsed");
 		$(".search__icon").addClass("search__icon--collapsed");
 		$(".search__input").hide();
+		$("#buttons__despre").hide();
 	}
 }
 
@@ -22,4 +26,6 @@ $(".top-menu__search").click( function() {
 	}, 500);
 	$(".top-menu__search").removeClass("top-menu__search--collapsed");
 	$(".search__icon").removeClass("search__icon--collapsed");
+	$(".top-menu__buttons").removeClass("top-menu__buttons--collapsed");
+	$("#buttons__despre").show();
 })
