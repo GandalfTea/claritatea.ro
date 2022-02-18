cae8b748f1ccebd58ddf68a1e11561d67a9bc249
gandalftea <octavian.concept@gmail.com>
2022-02-18 19:37:24 +0000

    begin mobile version / add carousel buttons and image authors

--- js/admin-ui.js
+++
@@ -1,7 +0,0 @@
-
-$('.news-field__buttons--edit').click(function() {
-    var parent = $(this).parent().parent();
-    var textarea = parent.getelementsbyclassname('.news-field__textfield')[0];
-    textarea.disabled = "true";
-});
-
--- js/transition.js
+++ js/transition.js
@@ -37,38 +37,58 @@ $(".top-menu__search").click( function() {
 })
 
 
-
-// MAGAZINE IMAGE FADE
-
-var counter = 0;
-setInterval( function() {
-	if (counter % 2 === 0) {
-		$('#main-presentation').fadeOut(1000);
-	} else {
-		$('#main-presentation').fadeIn(1000);
-	}
-
-	counter++;
-}, 6000);
-
-
-
 // LANDING IMAGE CAROUSEL
 
 const NUM_IMG = 9;
+const authors = [
+    "Vlas Cristina",
+    "Dionisie Cristea",
+    "Ana Balan",
+    "Delia Tarcan",
+    "Vlas Cristina",
+    "Anonim",
+    "Dimitru Mihaela-Laura",
+    "Ana Balan",
+    "Stiglet Cosmin",
+    "Matar Khaled"
+]
+
+const info = [
+    "Tempera pe hartie",
+    "Fotografie",
+    "Liner pe hartie",
+    "Fotografie",
+    "Natura Statica, Tempera pe panza",
+    "Tempera pe hartie",
+    "Ulei pe panza",
+    "Acuarela",
+    "Creion pe hartie",
+    "Fotografie"
+]
 
 idx = 0;
 var par = document.getElementById('background').parentNode;
 var cover = document.createElement('img');
-cover.classList.add('background-second');
+var change = false;
 cover.id = 'background-second';
 par.appendChild(cover);
 
-setInterval( () => {
-	console.log(idx)
-	idx = idx % NUM_IMG;
+function ticker() {
+    if(change) return;
+    console.log("ticker called");
+    setInterval( () => {
+        changeImage(idx);
+    }, 4000)
+}
+
+// callable from UI buttons and ticker()
+// reset ticker on call form button
+function changeImage(id, fromButton=false) {
+    if(fromButton) { change = true; }
+	idx = id % NUM_IMG;
 	var img = "../img/carousel/" + idx.toString() + ".jpg";	
 
+    // change image
 	if ( idx % 2) {
 		var img2 = '../img/carousel/' + (idx).toString() + '.jpg';
 		document.getElementById('background').src = img2;
@@ -77,10 +97,39 @@ setInterval( () => {
 		cover.src = img;
 		$('#background-second').fadeIn(1000)
 	}
+
+    // change author and title
+    document.getElementById("author-name").innerHTML = "<strong>" + authors[idx] + "</strong>";
+    document.getElementById("name").innerText = info[idx];
+
+    // change button selection
+    if(fromButton) {
+        console.log("CLEANING");
+        for(let j = 0; j > NUM_IMG; j++) {
+            console.log(j)
+            document.getElementById(j).style.backgroundColor = "#FFF";
+        }
+    }
+
+    if (id != 0) {
+        prev = id - 1
+        document.getElementById(prev).style.backgroundColor = "#FFF";
+    } else {
+        document.getElementById(NUM_IMG-1).style.backgroundColor = "#FFF";
+    }
+    document.getElementById(idx).style.backgroundColor = "#000";
+    
 	
 	idx += 1
+}
 
-}, 4000)
+if(change) {
+    change = false;
+    ticker();
+} else {
+    ticker();
+}
+
 
 
 
@@ -98,3 +147,21 @@ $(".wrapper__click-catch--right").click( function(event) {
 		scrollLeft: "+=500px"
 	}, "slow");
 });
+
+
+
+// MAGAZINE IMAGE FADE
+
+var counter = 0;
+setInterval( function() {
+	if (counter % 2 === 0) {
+		$('#main-presentation').fadeOut(1000);
+	} else {
+		$('#main-presentation').fadeIn(1000);
+	}
+
+	counter++;
+}, 6000);
+
+
+
